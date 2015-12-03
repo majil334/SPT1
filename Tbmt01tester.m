727d32f350cfb32713a9a0ce7f068ba2023528d8
Johanna Karlsson <Johanna@us-195-51.eduroam.liu.se>
2015-12-03 08:27:55 +0100

    Nu funkar det bra

--- Tbmt01tester.m
+++ Tbmt01tester.m
@@ -1,50 +1,3 @@
-f=0.5/360;
-
-[b,a]=butter(4,f, 'high');
-
-fvtool(b,a)
-
-fftad = fft(EKG3);
-
-figure(1)
-plot(EKG2)
-
-filtered = filtfilt(b,a,EKG3);
-
-figure(2)
-plot(abs(filtered))
-
-% timeagain = ifft(filtered,'symmetric');
-% 
-% figure(3)
-% plot(timeagain)
-
-[d,c]=butter(4,0.2,'low');
-
-lp = filtfilt(d,c,filtered);
-
-figure(3)
-plot(lp)
-
-
-
-
-%%
-med=mean(EKG1);
-
-
- for  i = 1 : 1 : length(EKG2) 
-     if  i == 1 
-         m(i)=EKG2(i+1)-EKG2(i); 
-     end
-     if  i > 1 
-         m(i)= EKG2(i)-EKG2(i-1); 
-     end
- end
- m=smooth(smooth(m));
- plot(m);
- 
- %%
 
 EKG = EKG3;
 fs = 360;
@@ -60,14 +13,12 @@ plot(EKGnice)
 minpeakheight = max(findpeaks(EKGnice))-0.8*max(findpeaks(EKGnice));
 
 
-[~,locs_Rwave] = findpeaks(EKGnice,'MinPeakHeight',minpeakheight,...
+[~,Rwave] = findpeaks(EKGnice,'MinPeakHeight',minpeakheight,...
     'MinPeakDistance',150);
 
 % size(pks)
-figure(2)
-% plot(locs_Rwave)
-size(locs_Rwave)
-beatspermin = length(locs_Rwave)/time*60
+size(Rwave)
+beatspermin = length(Rwave)/time*60
 
 
 %%
@@ -76,30 +27,54 @@ beatspermin = length(locs_Rwave)/time*60
 figure (5)
 hold on
 plot(EKGnice);
-plot(locs_Rwave,EKGnice(locs_Rwave),'rv','MarkerFaceColor','r');
+plot(Rwave,EKGnice(Rwave),'rv','MarkerFaceColor','r');
 
 
 
 %%
-A = [];
 
-for n=1:(length(locs_Rwave)- 1)
-    A(n) = locs_Rwave(n + 1)- locs_Rwave(n) ;
-end
+A = diff(Rwave);
+
 Rmean = mean(A);
 %% 
 arrythmias = 0;
-
-for n=1:(length(locs_Rwave)- 1)
-    if locs_Rwave(n + 1)- locs_Rwave(n) > Rmean + Rmean*0.2;
+loc_arr = zeros(length(Rwave),1);
+for n=1:(length(Rwave)- 1)
+    if Rwave(n + 1)- Rwave(n) > Rmean + Rmean*0.15;
         %display "arrythmia"
         arrythmias = arrythmias +1;
+        loc_arr(n) = Rwave(n);
+        loc_arr(n+1) = Rwave(n+1);
     end
-     if locs_Rwave(n + 1)- locs_Rwave(n) < Rmean - Rmean*0.2;
+     if Rwave(n + 1)- Rwave(n) < Rmean - Rmean*0.15;
   %display "arrythmia"
          arrythmias = arrythmias +1;
+                 loc_arr(n) = Rwave(n);
+        loc_arr(n+1) = Rwave(n+1);
      end
 end
 
-            arrythmias
+figure (6)
+hold on
+plot(EKGnice);
+plot(loc_arr,EKGnice(Rwave),'rv','MarkerFaceColor','r');
+%plot(loc_arr,EKGnice(loc_arr),'Marker','+','MarkerFaceColor','b');
 
+            arrythmias
+            
+%%
+
+m=mean(EKGnice(6*floor(beatspermin*time/fs):7*floor(beatspermin*time/fs))); %-0.07*Rmean;
+mvec = m*ones(length(EKGnice),1);
+
+figure(8)
+hold on
+plot(EKGnice(6*floor(beatspermin*time/fs):7*floor(beatspermin*time/fs)),'b')
+%plot(mvec,'r')
+
+%%
+N=127;%beatspermin*time/60;
+M=ceil(length(EKGnice)/N);
+beat=reshape(EKGnice,[N,M]);
+
+stem3(beat)
